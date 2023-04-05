a81a6be04eb605c53ce87be2bfee513f7cc73615
nesmaalabyad <92161296+nesmaalabyad@users.noreply.github.com>
2023-04-05 03:59:17 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,7 +7,9 @@ import 'constants/themes.dart';
 import 'providers/active_theme_provider.dart';
 import 'view/splashScreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: App()));
 }
 
