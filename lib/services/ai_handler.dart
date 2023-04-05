bb98bccf6cf0dd1a2e9579ab0ac493f0c962b8f5
nesmaalabyad <92161296+nesmaalabyad@users.noreply.github.com>
2023-04-05 04:00:02 +0200

    Update ai_handler.dart

--- lib/services/ai_handler.dart
+++ lib/services/ai_handler.dart
@@ -1,8 +1,10 @@
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
+import '../api/secrets.dart';
+
 class AIHandler {
   final _openAI = OpenAI.instance.build(
-    token: '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',
+    token: openAIAPIKey,
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
       connectTimeout: const Duration(seconds: 60),
@@ -13,7 +15,7 @@ class AIHandler {
     try {
       final request = ChatCompleteText(messages: [
         Map.of({"role": "user", "content": "I want you to act as a therapist and help me with the following. I want you to help me providing a safe space where I can talk to you and you act as a therapist responding to me and helping me overcome any problem. $message"})
-      ], maxToken: 200, model: kChatGptTurbo0301Model);
+      ], maxToken: 200, model: ChatModel.ChatGptTurbo0301Model);
 
       final response = await _openAI.onChatCompletion(request: request);
       if (response != null) {
